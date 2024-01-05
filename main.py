81f59c9f44997cb84f35f85384369266364c6812
ethnyl <jparkerpatton@gmail.com>
2024-01-05 10:37:19 -0600

    llllll

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # make key dict like this (from json?)
 key = {'challenge id': 'answer'}
 
-@app.route('/api/submit', methods=['POST'])
+@app.route('/submit', methods=['POST'])
 def validate():
     id = request.json['challenge'] # challenge name/id
     submit = request.json['submit'] # submitted answer from user
