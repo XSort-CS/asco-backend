33abefce7d1f5341c8e1503367e8329f92ac65a7
ethnyl <jparkerpatton@gmail.com>
2024-01-05 11:58:54 -0600

    ppp

--- main.py
+++ main.py
@@ -3,17 +3,50 @@ import os
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-# make key dict like this (from json?)
-key = {'challenge id': 'answer'}
+# json soonTM
+challenges = {}
+users = {}
 
-@app.route('/submit', methods=['POST'])
+class User:
+    def __init__(self, username, pwd):
+        self.username = username
+        self.password = pwd
+        self.score = 0
+
+    def add_score(n):
+        self.score += n
+
+class Challenge:
+    def __init__(self, cname, desc, answer, value):
+        self.cname = cname
+        self.desc = desc
+        self.answer = answer
+        self.value = value
+
+def add_challenge(cname, desc, answer):
+    challenges[cname] = Challenge(cname, desc, answer)
+
+@app.route('/register', methods=['POST'])
+def register(username, pwd):
+    if username in users:
+        return False
+    
+    users[username] = User(username, pwd)
+    return True
+
+
+@app.route('/submit', methods=['POST']) # not working
 def validate():
-    id = request.json['challenge'] # challenge name/id
-    submit = request.json['submit'] # submitted answer from user
+    cname = request.json['cname'] # challenge name/id
+    submitted_ans = request.json['submit'].lower # submitted answer from user
+    username = request.json['username']
 
-    submit = submit.lower
+    if submitted_ans == challenges[cname].answer:
+        users[username].add_score(challenges[cname].value)
+        return True
+    return False
+        
 
-    return submit == key[id]
 
 if __name__ == '__main__':
     app.run(debug=True)
